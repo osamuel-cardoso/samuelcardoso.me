3e98eb8f3193eb15834aebed2d65ac6ee08ebbb1
osamuel-cardoso <samuelcardoso555@gmail.com>
2024-10-12 01:29:36 -0300

    Update getWeather.ts

--- src/services/getWeather.ts
+++ src/services/getWeather.ts
@@ -1,9 +1,11 @@
 import { WeatherResponse } from '@/@types/weather'
 
-const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=Santa Rita, Paraíba&appid=2c9cb9dd5a93cb5c989980c52a4ecfec`
+const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=Santa Rita, Paraíba&appid=02eb2d0a404434a29b7a821b2fae1589`
 
 export async function getWeather() {
-    const response = await fetch(WEATHER_URL)
+    const response = await fetch(WEATHER_URL, {
+        cache: 'no-store',
+    })
     const data: WeatherResponse = await response.json()
 
     return data
